12daf6cde1b459b8d9970c06e2c8f4d789853c1f
seoyoungbyun <qustjdud2181@gmail.com>
2024-10-28 04:30:56 +0900

    jpa 기초 및 프로젝트 구조

---
+++ main/java/umc/spring/study/domain/Mission.java
@@ -0,0 +1,43 @@
+package umc.spring.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.study.domain.common.BaseEntity;
+import umc.spring.study.domain.mapping.UserMission;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(length = 70)
+    private String description;
+
+    @Column(nullable = false)
+    private Date deadline;
+
+    @Column(nullable = false)
+    private Integer point;
+
+    @Column(nullable = false)
+    private Long unique_code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<UserMission> userMissionList = new ArrayList<>();
+}
