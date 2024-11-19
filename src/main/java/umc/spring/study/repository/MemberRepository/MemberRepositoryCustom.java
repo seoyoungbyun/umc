9b231b10a365fd57720185cd738ed0054e64b494
seoyoungbyun <qustjdud2181@gmail.com>
2024-11-20 02:11:53 +0900

    github setting

---
+++ src/main/java/umc/spring/study/repository/MemberRepository/MemberRepositoryCustom.java
@@ -0,0 +1,8 @@
+package umc.spring.study.repository.MemberRepository;
+
+import umc.spring.study.domain.Member;
+
+public interface MemberRepositoryCustom {
+    Member findMemberById(Long memberId);
+    Member findRandomMember();
+}
---
+++ src/main/java/umc/spring/study/service/MemberService/MemberCommandService.java
@@ -0,0 +1,9 @@
+package umc.spring.study.service.MemberService;
+
+import umc.spring.study.domain.Member;
+import umc.spring.study.web.dto.MemberRequestDTO;
+
+public interface MemberCommandService {
+
+    Member joinMember(MemberRequestDTO.MemberJoinDto request);
+}
