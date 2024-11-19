8818c62f7675d9f317ac85906ba1f6ba76d81331
seoyoungbyun <qustjdud2181@gmail.com>
2024-11-20 02:52:48 +0900

    Revert "✨ [Feat]가게 추가 API 구현"
    This reverts commit 89450effe5f870f4968fed9348fb82ecb4122e07.

--- src/main/java/umc/spring/study/service/StoreService/StoreCommandService.java
+++
@@ -1,8 +0,0 @@
-package umc.spring.study.service.StoreService;
-
-import umc.spring.study.domain.Store;
-import umc.spring.study.web.dto.StoreRequestDTO;
-
-public interface StoreCommandService {
-    Store joinStore(StoreRequestDTO.StoreJoinDto request);
-}
